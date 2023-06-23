ae7e69a7038d35f55c570c56af48572554481bcb
mariemmezghani2023 <135975727+mariemmezghani2023@users.noreply.github.com>
2023-06-23 13:44:03 +0200

    create.php

---
+++ create.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AtelierBD1</title>
+</head>
+<body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "souk";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO article (name, price, quantity)
+VALUES ('".$_POST["name"]."', ".$_POST["price"].", ".$_POST["quantity"].")";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+</body>
+</html>
+
+
+
+
+
+
+
+
